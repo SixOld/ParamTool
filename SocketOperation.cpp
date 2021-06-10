a96207f8999a8632e8cd41cea7f7e0166076c494
范泓5 <fanhong5@hikvision.com>
2021-06-10 17:32:01 +0800

    feat(Socket): 添加Socket连接基础

--- SocketOperation.cpp
+++ SocketOperation.cpp
@@ -1,1 +1,47 @@
 #include "SocketOperation.h"
+
+C_SocketOperation::C_SocketOperation()
+{
+	m_socket = new QTcpSocket();
+	QObject::connect(m_socket, SIGNAL(QTcpSocket::readyRead()), this, SLOT(socket_Read_Data));
+	QObject::connect(m_socket, SIGNAL(QTcpSocket::disconnected()), this, SLOT(socket_Disconnected));
+}
+
+C_SocketOperation::~C_SocketOperation()
+{
+	if (m_socket != nullptr)
+	{
+		delete m_socket;
+	}
+}
+
+int32_t C_SocketOperation::ConnectToService(QString ip, int port)
+{
+	m_socket->abort();
+	m_socket->connectToHost(ip, port);
+
+	if (!m_socket->waitForConnected(10000))
+	{
+		qDebug() << "Connection failed!";
+		return -1;
+	}
+	return 0;
+}
+
+int32_t C_SocketOperation::BreakOffConnect()
+{
+	m_socket->disconnectFromHost();
+	return 0;
+}
+
+void C_SocketOperation::socket_Read_Data()
+{
+	//读取缓冲区数据
+	QByteArray buffer = m_socket->readAll();
+
+}
+
+void C_SocketOperation::socket_Disconnected()
+{
+
+}
--- SocketOperation.h
+++ SocketOperation.h
@@ -1,10 +1,24 @@
 #ifndef SOCKET_OPERATION_H
 #define SOCKET_OPERATION_H
 
+#include <QTcpSocket>
+#include <QString>
+#include <QObject>
 
-class SocketOperation
+class C_SocketOperation : public QObject
 {
+	Q_OBJECT
+public:
+	C_SocketOperation();
+	~C_SocketOperation();
+	int32_t ConnectToService(QString ip, int port);
+	int32_t BreakOffConnect();
+private:
+	QTcpSocket* m_socket = nullptr;
 
+private slots:
+	void socket_Read_Data();
+	void socket_Disconnected();
 };
 
 #endif
